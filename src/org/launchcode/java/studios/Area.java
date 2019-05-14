48a2461cfae8c54b6b07d6b94deae1502dc7b4cd
Chris <northodontist@gmail.com>
2019-05-13 20:11:33 -0500

    finished the studio, including both bonus missions

---
+++ src/org/launchcode/java/studios/Area.java
@@ -0,0 +1,28 @@
+package org.launchcode.java.studios;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        double userRadius;
+        java.util.Scanner in;
+
+        do {
+            in = new Scanner(System.in);
+            System.out.println("Enter the radius of your circle: ");
+            userRadius = in.nextDouble();
+            if (userRadius < 0) {
+                System.out.println("The radius of a circle cannot be negative!");
+            }
+        } while (userRadius < 0);
+        if (userRadius > 0) {
+            System.out.println(area(userRadius));
+        }
+    }
+
+    public static double area(double userRadius) {
+        double circleArea;
+        circleArea = 3.14 * userRadius * userRadius;
+        return (circleArea);
+    }
+}
