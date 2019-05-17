0ccd117ea3427f41e8dc919034a053bc0ad1b1c3
Chris <northodontist@gmail.com>
2019-05-16 20:10:26 -0500

    added some comments for better documentation

--- src/org/launchcode/java/studios/CountingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class CountingCharacters {
     public static void main(String[] args) {
 
+        //Get string from the user
         java.util.Scanner in;
         System.out.println("Enter the string you would like to count: ");
         in = new Scanner(System.in);
@@ -12,10 +13,13 @@ public class CountingCharacters {
         char[] charactersInString = characterString.toCharArray();
         HashMap<Character, Integer> characters = new HashMap<>();
 
+        //get each character while looping through string
         for(char i : charactersInString) {
             Integer total = charactersInString.length;
             Integer charCount = 0;
             char c = java.lang.Character.toLowerCase(i);
+
+            //count each character while looping through string
             for (int z = 0; z < total; z++){
                 if(c == java.lang.Character.toLowerCase(charactersInString[z])) {
                     charCount ++;
