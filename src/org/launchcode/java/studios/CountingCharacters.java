a7ed2a8ba0e981a59179f49f211175b927acef24
Chris <northodontist@gmail.com>
2019-05-16 19:55:29 -0500

    added the ability for the user to input their own string

--- src/org/launchcode/java/studios/CountingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters.java
@@ -1,9 +1,14 @@
 package org.launchcode.java.studios;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CountingCharacters {
     public static void main(String[] args) {
-        String characterString = "Aaa Bbbb CCCc";
+
+        java.util.Scanner in;
+        System.out.println("Enter the string you would like to count: ");
+        in = new Scanner(System.in);
+        String characterString = in.nextLine();
         char[] charactersInString = characterString.toCharArray();
         HashMap<Character, Integer> characters = new HashMap<>();
 
