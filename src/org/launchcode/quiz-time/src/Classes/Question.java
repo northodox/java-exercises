56fda875e799ab91b64066c30a79b782abf0a102
Chris <northodontist@gmail.com>
2019-06-06 18:54:49 -0500

    Added Quiz's constructor

--- src/org/launchcode/quiz-time/src/Classes/Question.java
+++ src/org/launchcode/quiz-time/src/Classes/Question.java
@@ -5,5 +5,9 @@ public abstract class Question {
     protected String questionText;
     protected String answer;
 
+    public Question(String questionText, String answer) {
+        this.questionText = questionText;
+        this.answer = answer;
+    }
 
 }
