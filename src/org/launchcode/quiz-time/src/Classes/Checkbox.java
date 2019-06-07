58521463d8c1972265fd8f7d1b8a3aed082ac136
Chris <northodontist@gmail.com>
2019-06-06 20:12:27 -0500

    Added Checkbox constructor

--- src/org/launchcode/quiz-time/src/Classes/Checkbox.java
+++ src/org/launchcode/quiz-time/src/Classes/Checkbox.java
@@ -1,4 +1,12 @@
 package Classes;
 
-public class Checkbox {
+public class Checkbox extends Question{
+
+    private String boxes;
+
+    public Checkbox(String questionText, String answer, String boxes) {
+        super(questionText, answer);
+        this.boxes = boxes;
+    }
+
 }
