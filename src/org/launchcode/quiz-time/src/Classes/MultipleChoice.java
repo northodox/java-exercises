38cd610a9422ecc314871fca292c3bc265a68155
Chris <northodontist@gmail.com>
2019-06-06 20:06:37 -0500

    Added extends keyword to MultipleChoice class constructor

--- src/org/launchcode/quiz-time/src/Classes/MultipleChoice.java
+++ src/org/launchcode/quiz-time/src/Classes/MultipleChoice.java
@@ -1,4 +1,11 @@
 package Classes;
 
-public class MultipleChoice {
+public class MultipleChoice extends Question {
+
+    private String choices;
+
+    public MultipleChoice(String questionText, String answer, String choices) {
+        super(questionText, answer);
+        this.choices = choices;
+    }
 }
