990e61eec90c56847ad3998932ab290f0dd568b0
Chris <northodontist@gmail.com>
2019-06-06 19:42:21 -0500

    Added receiveAnswer method to Question class, also generated equals method to help check answers

--- src/org/launchcode/quiz-time/src/Classes/Question.java
+++ src/org/launchcode/quiz-time/src/Classes/Question.java
@@ -1,24 +1,42 @@
 package Classes;
 
+
+import java.util.Objects;
+
 public abstract class Question {
 
     protected String questionText;
+
     protected String answer;
-    private int ID;
+    private int questionID;
 
     public Question(String questionText, String answer) {
         this.questionText = questionText;
         this.answer = answer;
+    }
 
+    public String receiveAnswer(String userAnswer) {
+        return userAnswer;
     }
 
     public Boolean checkAnswer(String userAnswer) {
-        if(answer.equals(userAnswer)) {
+        if (answer.equals(receiveAnswer(userAnswer))) {
             return true;
         }
         return false;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question question = (Question) o;
+        return answer.equals(question.answer);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(answer);
+    }
 
 }
